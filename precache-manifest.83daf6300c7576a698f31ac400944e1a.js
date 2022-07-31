acf904e4875718b8c504985175b4af0bfa1ee9fe
tarek-elias <tarek.elias97@gmail.com>
2022-07-31 15:57:37 +0400

    Updates

---
+++ precache-manifest.83daf6300c7576a698f31ac400944e1a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc423be7c271238476ec",
+    "url": "/tic-tac-toe/static/css/main.363bd2f9.chunk.css"
+  },
+  {
+    "revision": "dc423be7c271238476ec",
+    "url": "/tic-tac-toe/static/js/main.dc423be7.chunk.js"
+  },
+  {
+    "revision": "b1e84310496427886dbe",
+    "url": "/tic-tac-toe/static/js/1.b1e84310.chunk.js"
+  },
+  {
+    "revision": "26ead58317a649cdf406",
+    "url": "/tic-tac-toe/static/js/runtime~main.26ead583.js"
+  },
+  {
+    "revision": "02af7c1702f34ebc78ac11f944ac9072",
+    "url": "/tic-tac-toe/index.html"
+  }
+];
